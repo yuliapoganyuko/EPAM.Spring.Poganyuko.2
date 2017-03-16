3da5bed5f3b9299be253ca83d871ba260dbd123f
Yulia Poganyuko <yulia.poganyuko@gmail.com>
2017-03-17 01:21:47 +0300

    Add Task2Logic with class Gcd for counting greatest common divisor using Euclid's algorithm and binary algorithm

---
+++ Task2Logic/Gcd.cs
@@ -0,0 +1,176 @@
+﻿using System;
+
+namespace Task2Logic
+{
+    /// <summary>
+    /// Provides functionality for getting a greatest common divisor.
+    /// </summary>
+    public static class Gcd
+    {
+        #region Public methods
+
+        /// <summary>
+        /// Counts a greatest common divisor for the pair of numbers using Euclid's algorithm.
+        /// </summary>
+        /// <param name="a">First number</param>
+        /// <param name="b">Second number</param>
+        /// <returns>Greatest common divisor</returns>
+        public static int GetGcd(int a, int b)
+        {
+            if (a == 0 && b == 0)
+                throw new ArgumentException();
+            return GetEuclidGcd(a,b);
+        }
+
+        /// <summary>
+        /// Counts a greatest common divisor for 3 and more numbers using Euclid's algorithm.
+        /// </summary>
+        /// <param name="a">First number</param>
+        /// <param name="b">Second number</param>
+        /// <param name="array">Other numbers</param>
+        /// <returns>Greatest common divisor</returns>
+        public static int GetGcd(int a, int b, params int[] array)
+        {
+            if (array == null)
+                throw new ArgumentNullException();
+            if (array.Length == 0)
+                GetGcd(a, b);
+            int i = 0;
+            while (i < array.Length && array[i] == 0)
+                i++;
+            if (i == array.Length)
+                throw new ArgumentException();
+            return GetEuclidGcd(a, b, array);
+        }
+
+        /// <summary>
+        /// Counts a greatest common divisor for the pair of numbers using binary algorithm.
+        /// </summary>
+        /// <param name="a">First number</param>
+        /// <param name="b">Second number</param>
+        /// <returns>Greatest common divisor</returns>
+        public static int GetBinaryGcd(int a, int b)
+        {
+            if (a == 0 && b == 0)
+                throw new ArgumentException();
+            return GetGcdByBinaryAlgorithm(a, b);
+        }
+
+        /// <summary>
+        /// Counts a greatest common divisor for 3 and more numbers using binary algorithm.
+        /// </summary>
+        /// <param name="a">First number</param>
+        /// <param name="b">Second number</param>
+        /// <param name="array">Other numbers</param>
+        /// <returns>Greatest common divisor</returns>
+        public static int GetBinaryGcd(int a, int b, params int[] array)
+        {
+            if (array == null)
+                throw new ArgumentNullException();
+            if (array.Length == 0)
+                GetGcd(a, b);
+            int i = 0;
+            while (i < array.Length && array[i] == 0)
+                i++;
+            if (i == array.Length)
+                throw new ArgumentException();
+            return GetGcdByBinaryAlgorithm(a, b, array);
+        }
+        #endregion
+
+        #region Private methods
+
+        /// <summary>
+        /// Counts a greatest common divisor for the pair of numbers using Euclid's algorithm.
+        /// </summary>
+        /// <param name="a">First number</param>
+        /// <param name="b">Second number</param>
+        /// <returns>Greatest common divisor</returns>
+        private static int GetEuclidGcd(int a, int b)
+        {
+            while (a != b)
+            {
+                if (a > b)
+                    a = a - b;
+                else
+                    b = b - a;
+            }
+            return a;
+        }
+
+        /// <summary>
+        /// Counts a greatest common divisor for 3 and more numbers using Euclid's algorithm.
+        /// </summary>
+        /// <param name="a">First number</param>
+        /// <param name="b">Second number</param>
+        /// <param name="array">Other numbers</param>
+        /// <returns>Greatest common divisor</returns>
+        private static int GetEuclidGcd(int a, int b, params int[] array)
+        {
+            a = GetEuclidGcd(a, b);
+            for (int i = 0; i < array.Length; i++)
+            {
+                a = GetEuclidGcd(a, array[i]);
+            }
+            return a;
+        }
+
+        /// <summary>
+        /// Counts a greatest common divisor for the pair of numbers using binary algorithm.
+        /// </summary>
+        /// <param name="a">First number</param>
+        /// <param name="b">Second number</param>
+        /// <returns>Greatest common divisor</returns>
+        private static int GetGcdByBinaryAlgorithm(int a, int b)
+        {
+            if (a == b)
+                return a;
+            if (a == 0)
+                return b;
+            if (b == 0)
+                return a;
+            //if ((~a & 1) != 0)
+            //{
+            //    if ((b & 1) != 0)
+            //        return GetBinaryGcd(a >> 1, b);
+            //    else
+            //        return GetBinaryGcd(a >> 1, b >> 1) << 1;
+            //}
+            //if ((~b & 1) != 0)
+            //    return GetBinaryGcd(a, b >> 1);
+            //if (a > b)
+            //    return GetBinaryGcd((a - b) >> 1, b);
+            //return GetBinaryGcd((b - a) >> 1, a);
+            if ((~a & 1) != 0)
+            {
+                if ((b & 1) != 0)
+                    return GetBinaryGcd(a >> 1, b);
+                return GetBinaryGcd(a >> 1, b >> 1) << 1;
+            }
+            if ((~b & 1) != 0)
+                return GetBinaryGcd(a, b >> 1);
+            if (a > b)
+                return GetBinaryGcd((a - b) >> 1, b);
+            return GetBinaryGcd((b - a) >> 1, a);
+        }
+
+        /// <summary>
+        /// Counts a greatest common divisor for 3 and more numbers using binary algorithm.
+        /// </summary>
+        /// <param name="a">First number</param>
+        /// <param name="b">Second number</param>
+        /// <param name="array">Other numbers</param>
+        /// <returns>Greatest common divisor</returns>
+        private static int GetGcdByBinaryAlgorithm(int a, int b, params int[] array)
+        {
+            a = GetGcdByBinaryAlgorithm(a, b);
+            for (int i = 0; i < array.Length; i++)
+            {
+                a = GetGcdByBinaryAlgorithm(a, array[i]);
+            }
+            return a;
+        }
+        #endregion
+
+    }
+    }
